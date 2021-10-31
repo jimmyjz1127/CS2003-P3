390cfd5c4942de2f259fcf53c14d92704f870b78
jimmyjz1127 <jimmyjz1127@gmail.com>
2021-10-31 19:38:43 +0000

    -Moved properties file to directory "propertiesFiles" -Created Configuration class with basic property attribute initialization -Created DMBServer.java class -Created directory "messageFile" for storing message files created by DMBServer.java

---
+++ src/Configuration.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.net.*;
+import java.util.Properties;
+
+public class Configuration
+{
+    //Setup
+    public Properties properties;
+    public String propertiesFile = "propertiesFiles/DMBProperties.properties";
+    public LogFileWriter logWriter;
+
+
+    //Attributes
+    public String serverAddress;
+    public int serverPort;
+    public String directoryFile;
+    public String boardDirectory;
+    public String logFile;
+
+    public Configuration(String file)
+    {
+        //if a different properties file is passed to constructor
+        if (file != null)
+        {
+            propertiesFile = file; 
+        }
+
+        try
+        {
+            properties = new Properties();
+            InputStream p = getClass().getClassLoader().getResourceAsStream(propertiesFile);
+            
+            if (p != null)
+            {
+                //load data from properties file
+                properties.load(p);
+                String property;
+                
+                //initialize property attributes
+                if((property = properties.getProperty("serverAddress")) != null)
+                {
+                    serverAddress = new String(property);
+                }
+
+                if ((property = properties.getProperty("serverPort")) != null)
+                {
+                    serverPort = Integer.parseInt(property);
+                }
+
+                if ((property = properties.getProperty("directoryFile")) != null)
+                {
+                    directoryFile = new String(property);
+                }
+
+                if ((property = properties.getProperty("boardDirectory")) != null)
+                {
+                    boardDirectory = new String(property);
+                }
+
+                if ((property = properties.getProperty("logFile")) != null)
+                {
+                    logFile = new String(property);
+                }
+
+                p.close();
+            }
+
+            logWriter = new LogFileWriter(logFile);
+        }
+        catch (NumberFormatException e)
+        {
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        
+    }
+}
